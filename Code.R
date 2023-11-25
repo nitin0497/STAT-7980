869aef99dc7eeb054e862647da3b64e11c6e38ab
Nitin yadav <nitinyadav0497@gmail.com>
2023-11-25 14:49:48 -0600

    added the latest changes.

--- Code.R
+++ Code.R
@@ -62,12 +62,12 @@ simulate_GARCH <- function(n, omega, alpha, beta = 0) {
   return(list(resid = resid, variance = variance))
 }
 
-
+n=200
 # Simulate an ARCH(1) series
-arch <- simulate_GARCH(n = 200, omega = 0.1, alpha = 0.7)
+arch <- simulate_GARCH(n = n, omega = 0.1, alpha = 0.7)
 
 # Simulate a GARCH(1,1) series
-garch <- simulate_GARCH(n = 200, omega = 0.1, alpha = 0.7, beta = 0.3)
+garch <- simulate_GARCH(n = n, omega = 0.1, alpha = 0.7, beta = 0.3)
 
 # Create a data frame for plotting
 data <- data.frame(
@@ -84,92 +84,25 @@ ggplot(data, aes(x = index)) +
   labs(x = "Time", y = "Variance", color = "Variance Type") +
   theme_minimal()
 
-# ####Impact of Beta####
-# # First simulated GARCH
-# first_garch <- simulate_GARCH(n = 200, omega = 0.1, alpha = 0.3, beta = 0.2)
-# first_data <- data.frame(
-#   index = 1:200,
-#   variance = first_garch$variance,
-#   residuals = first_garch$resid
-# )
-# 
-# # Plot first simulated GARCH using ggplot2
-# ggplot(first_data, aes(x = index)) +
-#   geom_line(aes(y = variance, color = "Variance")) +
-#   geom_line(aes(y = residuals, color = "Residuals")) +
-#   scale_color_manual(values = c("Variance" = "red", "Residuals" = "deepskyblue")) +
-#   labs(x = "Time", y = "Value", color = "Variable") +
-#   ggtitle("First simulated GARCH, Beta = 0.2") +
-#   theme_minimal()
-# 
-# # Second simulated GARCH
-# second_garch <- simulate_GARCH(n = 200, omega = 0.1, alpha = 0.3, beta = 0.7)
-# second_data <- data.frame(
-#   index = 1:200,
-#   variance = second_garch$variance,
-#   residuals = second_garch$resid
-# )
-# 
-# # Plot second simulated GARCH using ggplot2
-# ggplot(second_data, aes(x = index)) +
-#   geom_line(aes(y = variance, color = "Variance")) +
-#   geom_line(aes(y = residuals, color = "Residuals")) +
-#   scale_color_manual(values = c("Variance" = "red", "Residuals" = "deepskyblue")) +
-#   labs(x = "Time", y = "Value", color = "Variable") +
-#   ggtitle("Second simulated GARCH, Beta = 0.7") +
-#   theme_minimal()
-# 
-# 
-# ####optimal weights ####
-# 
-# # Define symbols for the three stock indices
-# symbols <- c("^GSPC", "^IXIC", "^DJI")
-# 
-# # Get historical data for the stock indices
-# getSymbols(symbols, from = "2012-01-01", to = "2022-01-01", src = "yahoo")
-# 
-# # Extract adjusted closing prices for the three indices
-# sp500_prices <- Cl(GSPC)
-# nasdaq_prices <- Cl(IXIC)
-# dow_prices <- Cl(DJI)
-# 
-# # Calculate daily returns for the three indices
-# sp500_returns <- diff(log(sp500_prices))
-# nasdaq_returns <- diff(log(nasdaq_prices))
-# dow_returns <- diff(log(dow_prices))
-# 
-# # Create a data frame for the portfolio returns
-# portfolio <- data.frame(SP500_Returns = sp500_returns,
-#                                 NASDAQ_Returns = nasdaq_returns,
-#                                 DowJones_Returns = dow_returns)
-# #rownames(portfolio)=index(sp500_returns)
-# portfolio=portfolio[-1,]
-# 
-# # Create the portfolio specification
-# port_spec <- portfolio.spec(colnames(portfolio))
-# 
-# # Add a full investment constraint such that the weights sum to 1
-# port_spec <- add.constraint(portfolio =port_spec, type = "full_investment")
-# 
-# # Add a long only constraint such that the weight of an asset is between 0 and 1
-# port_spec <- add.constraint(portfolio = port_spec, type = "long_only")
-# 
-# # Add an objective to minimize portfolio standard deviation
-# port_spec <- add.objective(portfolio = port_spec, type = "risk", name = "StdDev")
-# 
-# # Solve the optimization problem
-# opt <- optimize.portfolio(portfolio, portfolio = port_spec, optimize_method = "ROI")
-# 
-# #optimal weights
-# weights=opt$weights
-# 
-# # Calculate weighted portfolio returns
-# portfolio$P_ret <- weights[1] * portfolio[,1] +
-#   weights[2] * portfolio[,2] +
-#   weights[3] * portfolio[,3]
-# 
-# portfolio$date=rownames(portfolio)
-# #portfolio$Date=rownames(portfolio)
+
+#extracting the model coefficients to verify
+#ARCH
+sim_arch_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,0)),
+                         mean.model = list(armaOrder = c(0,0), include.mean = TRUE),
+                         distribution.model = "norm")
+
+sim_arch_model <- ugarchfit(data = arch$resid, spec = sim_arch_spec)
+sim_arch_model@fit[["matcoef"]][,1]
+
+#GARCH
+sim_garch_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)),
+                         mean.model = list(armaOrder = c(0,0), include.mean = TRUE),
+                         distribution.model = "norm")
+
+# Fit the model
+sim_garch_model <- ugarchfit(data = garch$resid, spec = sim_garch_spec)
+sim_garch_model@fit[["matcoef"]][,1]
+
 
 ####BP-test,ADF test####
 adf.test(sp500_returns)
@@ -178,11 +111,11 @@ adf.test(sp500_returns)
 lm_model <- lm(sp500_returns ~ lag(sp500_returns, 1) + lag(sp500_returns, 2), data = sp500_returns)
 
 # Perform the Breusch-Pagan test
-bptest_result <- bptest(lm_model)
-bptest_result
+bptest(lm_model)
 
 
-####Basic GARCH model with skewed t-distribution####
+
+####GJR-GARCH model with skewed t-distribution####
 
 # Specify GARCH model assumptions
 garch_spec <- ugarchspec(variance.model = list(model = "gjrGARCH", garchOrder = c(1,1)),
@@ -201,8 +134,8 @@ garch_vol=garch_model@fit[["sigma"]]
 
 par(mfrow=c(1,1))
 ggplot(sp500_returns, aes(x = index(sp500_returns))) +
-  geom_line(aes(y = garch_vol, color = "blue"), size = 0.5, linetype = 1, show.legend = TRUE,) +
-  geom_line(aes(y = sp500_returns, color = "red"), size = 0.5, linetype = 1, show.legend = TRUE, alpha = 0.4) +
+  geom_line(aes(y = garch_vol, color = "blue"), linewidth = 0.5, linetype = 1, show.legend = TRUE,) +
+  geom_line(aes(y = sp500_returns, color = "red"), linewidth = 0.5, linetype = 1, show.legend = TRUE, alpha = 0.4) +
   scale_color_manual(values = c("red", "blue"),
                      labels = c("Volatility", "Returns")) +
   labs(title = "GJR-GARCH(1,1) Model with Skewed-t Distribution", x = "Date", y = "Value") +
@@ -211,25 +144,22 @@ ggplot(sp500_returns, aes(x = index(sp500_returns))) +
 
 
 ####Rolling Forecast####
-n_start=2000
+n_start=252*5
 garchroll<-ugarchroll(garch_spec, data = sp500_returns,n.start =n_start, 
-                      refit.window="moving", refit.every =10, calculate.VaR = TRUE, VaR.alpha = c(0.01,0.05))
+                      refit.window="moving", refit.every =5, calculate.VaR = TRUE, VaR.alpha = c(0.01,0.05))
 
 report(garchroll)
-#coef(garchroll)
 preds<-as.data.frame(garchroll)
 
 
-plot(abs(preds$Realized), type = "p", col = "blue", xlab = "Observation", ylab = "vol", main = "GARCH vol vs Realized vol")
-lines(x = preds$Sigma, col = "red", type="l")
-lines(x =  col = "black", type="l")
+# plot(abs(preds$Realized), type = "l", col = "blue", xlab = "Observation", ylab = "vol", main = "GARCH vol vs Realized vol")
+# lines(x = preds$Sigma, col = "red", type="l")
+# lines(x =  col = "black", type="l")
 
-# #MAE
-# e=(preds$Realized-preds$Sigma)/preds$Sigma
-# mean(abs(e))
-# #MSE
-# mse=(e)^2
-# mean(abs(e))
+#MSE
+e=(preds$Realized-preds$Sigma)/preds$Sigma
+mean(e^2)
+
 
 ####VaR####
 var_1=garchroll@forecast[["VaR"]][["alpha(1%)"]]
@@ -252,13 +182,12 @@ lines(var_5, col = "red", type="l")  # Add VaR line
 
 # Mark exceedance points
 points(exceedance_points5, returns[exceedance_points5], col = "red", pch = 16)
-points(exceedance_points1, returns[exceedance_points1], col = "green", pch = 16)
+points(exceedance_points1, returns[exceedance_points1], col = "black", pch = 16)
 
 # Add legend
 legend("topright", legend = c("Returns", "1% VaR","5% VaR", "1% Exceedance","5% Exceedance"), 
-       col = c("blue", "green", "red","green","red"), lty = c(1, 1, 1,NA,NA), pch = c(NA, NA, NA,16,16))
-#legend("bottomright",legend=c(paste0("1% exceedance ",length(exceedance_points1)),
- #                             paste0("5% exceedance ",length(exceedance_points5))))
+       col = c("blue", "green", "red","black","red"), lty = c(1, 1, 1,NA,NA), pch = c(NA, NA, NA,16,16))
+legend("bottomright",legend=c(paste0("1% exceedance ",length(exceedance_points1))))
 
 
 
